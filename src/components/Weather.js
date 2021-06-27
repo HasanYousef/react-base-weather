b4d209f53d4273944d04e30191c911c4098f8f25
HasanYousef <hasan.yousef.cs@gmail.com>
2021-06-27 15:07:36 +0300

    last

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { makeStyles } from '@material-ui/core/styles';
 
 import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
@@ -7,49 +8,93 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 import WelcomeHeader from './WelcomeHeader';
 
+const useStyles = makeStyles({
+  root: {
+    marginBottom: 60,
+  },
+  unitSelect: {
+    display: 'flex',
+    marginTop: 20,
+    marginBottom: 20,
+    alignItems: 'center',
+  },
+  unitLabel: {
+    marginRight: 10,
+  },
+  card: {
+    marginTop: 20,
+  }
+});
+
 const Weather = (props) => {
-  const [city, setCity] = useState("");
+  const classes = useStyles();
+
+  const [city, setCity] = useState("Tel Aviv");
   const [weather, setWeather] = useState({});
+  const [weekWeather, setWeekWeather] = useState({});
   const [unit, setUnit] = useState('metric');
 
-  const units = ['standard', 'metric', 'imperial'];
+  const units = ['metric', 'imperial'];
 
-  const fetchData = (name) => {
+  const fetchData = async (name) => {
     const key = "0ab91e9196d91e6a41cca4ce35e397f5";
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${key}&units=${unit}`;
-    axios.get(url).then((response) => {
+    const getCurrUrl = `http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${key}&units=${unit}`;
+    const getWeekUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${name}&appid=${key}&units=${unit}`;
+
+    const fetchByUrl = async (url, setData) => {
+      let response = await axios.get(url);
       if (response && response.data && response.status === 200) {
-        setWeather(response.data);
+        setData(response.data);
+        console.log(response.data);
       }
-    });
+    }
+
+    await fetchByUrl(getCurrUrl, setWeather);
+    await fetchByUrl(getWeekUrl, setWeekWeather);
+
   };
 
   useEffect(() => {
     fetchData(city);
-    console.log(weather.main);
   }, [city, unit]);
 
   return (
     <div>
       <WelcomeHeader />
-      <TextField onChange={(event) => setCity(event.target.value)} label="City name" />
-      <Box>
-        <InputLabel id="select-unit">Unit</InputLabel>
+      <TextField value={city} onChange={(event) => setCity(event.target.value)} label="City name" />
+      <Box className={classes.unitSelect}>
+        <InputLabel id="select-unit" className={classes.unitLabel}>Unit</InputLabel>
         <Select
           labelId="select-unit"
           value={unit}
           onChange={(event) => setUnit(event.target.value)}
         >
           {
-            units.map(u => <MenuItem value={u}>{u}</MenuItem>)
+            units.map(u => <MenuItem key={u} value={u}>{u}</MenuItem>)
           }
         </Select>
       </Box>
       {
-        weather.main && <Typography>{weather.main.temp}</Typography>
+        weather.main && <Typography>{`Current weather in ${city}: ${weather.main.temp} ${unit === 'metric' ? '°C' : '°F'}`}</Typography>
+      }
+
+      {
+        (weekWeather && weekWeather.list) &&
+        weekWeather.list.map(row => (
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography>{row.main.temp + (unit === 'metric' ? '°C' : '°F')}</Typography>
+              {
+                (new Date(row.dt_txt)).toLocaleDateString()
+              }
+            </CardContent>
+          </Card>
+        ))
       }
     </div>
   );
